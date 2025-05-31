97d3bbc20ab1cbdd62558dfe551443d3f113ee88
juaneraso <juangui9508@hotmail.com>
2025-05-31 10:59:09 -0500

    feature print

--- prueba.py
+++ prueba.py
@@ -1,3 +1,6 @@
 print("hola mundo")
 
 nombre = input("Ingrese su nombre:")
+
+print("Hola campistas estamos haciendo una colaboracion en github")
+print("hola Pablo, Hola Orlando ,Hola Eder , Hola Blanca , Hola Diego")
