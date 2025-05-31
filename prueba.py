874b9985ee2913bc08a0be542c0879f172d8078c
createcommand <pportilla2@gmail.com>
2025-05-31 11:02:06 -0500

    pablo

--- prueba.py
+++ prueba.py
@@ -4,3 +4,4 @@ nombre = input("Ingrese su nombre:")
 
 print("Hola campistas estamos haciendo una colaboracion en github")
 print("hola Pablo, Hola Orlando ,Hola Eder , Hola Blanca , Hola Diego")
+print("Soy Pablo y este es el primer cambio que realizo")
