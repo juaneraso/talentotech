ac92fbfe750cefdf0c11666e3e8391839e7b33e3
orlandofc22 <orlandofigueroa4@hotmail.com>
2025-05-31 11:06:32 -0500

    orlando

--- prueba.py
+++ prueba.py
@@ -5,3 +5,5 @@ nombre = input("Ingrese su nombre:")
 print("Hola campistas estamos haciendo una colaboracion en github")
 print("hola Pablo, Hola Orlando ,Hola Eder , Hola Blanca , Hola Diego")
 print("Soy Pablo y este es el primer cambio que realizo")
+print("Soy Orlando y este es el primer cambio que realizo")
+
